3fea1a54022ace755ce4c7dea0226d46d84f93c6
bhhupender <bhhupender@gmail.com>
2024-05-18 13:53:11 +0530

    first commit

---
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react'
+import { createEmployee } from '../services/EmployeeService'
+import {useNavigate} from 'react-router-dom'
+
+const EmployeeComponent = () => {
+
+   const [firstName, setFirstName] = useState('')
+   const [lastName, setLastName] = useState('')
+   const [email, setEmail] = useState('')
+   
+   const [errors, setErrors] = useState( {
+            firstName:'',
+            lastName:'',
+            email:''
+   })
+   const navigator = useNavigate();
+   
+   function handleFirstName(e){
+        setFirstName(e.target.value);
+   }
+   function handleLastName(e){
+    setLastName(e.target.value);
+}
+function handleEmail(e){
+    setEmail(e.target.value);
+}
+function saveEmployee(e){
+    e.preventDefault();
+
+    if(validateForm()){
+        const employee = {firstName, lastName, email}
+        console.log(employee)
+
+        createEmployee(employee).then((response) => {
+        console.log(response.data);
+        navigator('/employees')
+    })
+    }
+    
+}
+    function validateForm(){
+        let valid = true;
+        const errorsCopy = {... errors}
+
+        if(firstName.trim()){
+            errorsCopy.firstName = '';
+        }else{
+            errorsCopy.firstName = 'First Name is required';
+            valid = false;
+        }
+        if(lastName.trim()){
+            errorsCopy.lastName = '';
+        }else{
+            errorsCopy.lastName = 'Last Name is required';
+            valid = false;
+        }
+        if(email.trim()){
+            errorsCopy.email = '';
+        }else{
+            errorsCopy.email = 'Email is required';
+            valid = false;
+        }
+        setErrors(errorsCopy);
+        return valid;
+    }
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='card'>
+                <h2 className='text-center'>Add Employee</h2>
+                <div className='card-body'>
+                    <form>
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>First Name</label>
+                            <input
+                                type='text'
+                                placeholder='Enter Employee First Name'
+                                name='firstName'
+                                value={firstName}
+                                className={`form-control ${ errors.firstName ? 'is-invalid': ''}`}
+                                onChange={handleFirstName}
+                            >
+                            </input>
+                            { errors.firstName && <div className='invalid-feedback'>{errors.firstName} </div>}
+                        </div>
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Last Name</label>
+                            <input
+                                type='text'
+                                placeholder='Enter Employee Last Name'
+                                name='lastName'
+                                value={lastName}
+                                className={`form-control ${ errors.lastName ? 'is-invalid': ''}`}
+                                onChange={handleLastName}
+                            >
+                            </input>
+                            { errors.lastName && <div className='invalid-feedback'>{errors.lastName} </div>}
+                        </div>
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Email</label>
+                            <input
+                                type='text'
+                                placeholder='Enter Employee Email'
+                                name='email'
+                                value={email}
+                                className={`form-control ${ errors.email ? 'is-invalid': ''}`}
+                                onChange={handleEmail}
+                            >
+                            </input>
+                            { errors.email && <div className='invalid-feedback'>{errors.email} </div>}
+                        </div>
+                        <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default EmployeeComponent
